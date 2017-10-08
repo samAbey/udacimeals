6cefabc9da54dfd399b0dc64d04bdd27a122283f
Sameera Chathuranga <studies.sameera@gmail.com>
2017-10-08 20:29:37 +0530

    Add another reducer to learn reducer composition

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -41,6 +41,20 @@ const initialCalendarState = {
   },
 }
 
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
+
 function calendar (state = initialCalendarState, action) {
   const { day, recipe, meal} = action;
 
